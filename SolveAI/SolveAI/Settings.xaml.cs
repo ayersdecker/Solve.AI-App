1d613babe3c32a167c8ac528b9a4a8d69df97a67
Decker Ayers <ayersdecker@gmail.com>
2023-07-03 15:05:04 -0400

    Basic Structure

---
+++ SolveAI/SolveAI/Settings.xaml.cs
@@ -0,0 +1,19 @@
+namespace SolveAI;
+
+public partial class Settings : ContentPage
+{
+	public Settings()
+	{
+		InitializeComponent();
+	}
+
+    private void Save_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OpenAiInfoButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
