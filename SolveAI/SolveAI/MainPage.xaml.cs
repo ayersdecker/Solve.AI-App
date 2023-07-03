7ac42425f67c34505998b6394603d41f6cea9f8a
Decker Ayers <ayersdecker@gmail.com>
2023-07-03 15:33:25 -0400

    Hyperlinks Added

--- SolveAI/SolveAI/MainPage.xaml.cs
+++ SolveAI/SolveAI/MainPage.xaml.cs
@@ -1,15 +1,37 @@
-﻿namespace SolveAI;
+﻿
+
+namespace SolveAI;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
+    public string APIKey;
 	public MainPage()
 	{
 		InitializeComponent();
-
     }
 
-	
+    private void Submit_Clicked(object sender, EventArgs e)
+    {
+
+
+
+
+    }
+    private void ReadAPIKey()
+    {
+        var path = FileSystem.Current.AppDataDirectory;
+        var fullPath = Path.Combine(path, "APIKey.txt");
+        if (File.Exists(fullPath))
+        {
+            APIKey = File.ReadAllText(fullPath);
+        }
+        else
+        {
+            DisplayAlert("API Key", "You need to save a valid API Key in your settings tab!", "Yessir");
+        }
+        
+    }
+
 }
 
--- SolveAI/SolveAI/Settings.xaml.cs
+++ SolveAI/SolveAI/Settings.xaml.cs
@@ -1,3 +1,8 @@
+using System.Threading.Tasks;
+using System.Threading;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Alerts;
+
 namespace SolveAI;
 
 public partial class Settings : ContentPage
@@ -7,13 +12,30 @@ public partial class Settings : ContentPage
 		InitializeComponent();
 	}
 
-    private void Save_Clicked(object sender, EventArgs e)
+    // User Clicks Save Button
+    private async void Save_Clicked(object sender, EventArgs e)
     {
-
+        var path = FileSystem.Current.AppDataDirectory;
+        var fullPath = Path.Combine(path, "APIKey.txt");
+        string apiKey = ApiKeyEntry.Text;
+        if (apiKey == "" || apiKey == "API KEY HERE")
+        {
+            await DisplayAlert("Error", "Please enter a valid API Key", "OK");
+            return;
+        }
+        else
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            string text = "API Key Saved";
+            ToastDuration duration = ToastDuration.Short;
+            double fontSize = 14;
+            var toast = Toast.Make(text, duration, fontSize);
+            await toast.Show(cancellationTokenSource.Token);
+        }
     }
-
+    // User Clicks OpenAI Info Button
     private void OpenAiInfoButton_Clicked(object sender, EventArgs e)
     {
-
+        Launcher.OpenAsync("https://platform.openai.com/account/api-keys");
     }
 }
